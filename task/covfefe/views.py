2a35bdc86b61f85a3dc55acb8109c0b0488b7272
swiatlamiasta <arturziomas@gmail.com>
2018-08-24 13:34:39 +0200

    made urls in templates DRY

--- task/covfefe/views.py
+++ task/covfefe/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render, redirect
 from .models import Provider, Offer
 from .forms import ProviderForm
 
-def index(request, template_name='covfefe/index.html'):
+def index(request, template_name='covfefe/index.html', name='index'):
     return render(request, template_name, {})
 
-def providers(request, template_name='covfefe/providers.html'):
+def providers(request, template_name='covfefe/providers.html', name='providers'):
     providers = Provider.objects.all()
     return render(request, template_name, {'providers': providers})
 
-def offers(request, template_name='covfefe/offers.html'):
+def offers(request, template_name='covfefe/offers.html', name='offers'):
     offers = Offer.objects.all()
     return render(request, template_name, {'offers': offers})
 
-def add_provider(request, template_name='covfefe/add_provider.html'):
+def add_provider(request, template_name='covfefe/add_provider.html', name='add_provider'):
     form = ProviderForm(request.POST or None)
 
     if form.is_valid():
