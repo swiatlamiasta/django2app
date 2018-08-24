41cfd884d1bc2d43cea96c1f38c23f9ef8e52157
swiatlamiasta <arturziomas@gmail.com>
2018-08-24 09:08:13 +0200

    initial db migration

--- task/covfefe/views.py
+++ task/covfefe/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request, template_name='covfefe/index.html'):
-    return render(request, template_name, {'test': test})
+    return render(request, template_name, {})
