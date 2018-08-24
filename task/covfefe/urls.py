ad5393f9947e6016700f3dc45fb1fb6750342cd5
swiatlamiasta <arturziomas@gmail.com>
2018-08-24 10:37:52 +0200

    templates added

--- task/covfefe/urls.py
+++ task/covfefe/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('providers/', views.providers, name='providers'),
+    path('offers/', views.offers, name='offers'),
 ]
--- task/covfefe/views.py
+++ task/covfefe/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request, template_name='covfefe/index.html'):
     return render(request, template_name, {})
+
+def providers(request, template_name='covfefe/providers.html'):
+    return render(request, template_name, {})
+
+def offers(request, template_name='covfefe/offers.html'):
+    return render(request, template_name, {})
