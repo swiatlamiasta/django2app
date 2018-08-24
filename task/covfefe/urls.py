ffe086d39b0bbd711c013aca47af16e65b8de2da
swiatlamiasta <arturziomas@gmail.com>
2018-08-24 11:43:49 +0200

    templates improved a bit

--- task/covfefe/urls.py
+++ task/covfefe/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('providers/', views.providers, name='providers'),
     path('offers/', views.offers, name='offers'),
 ]
--- task/covfefe/views.py
+++ task/covfefe/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Provider, Offer
 
 def index(request, template_name='covfefe/index.html'):
     return render(request, template_name, {})
 
 def providers(request, template_name='covfefe/providers.html'):
-    return render(request, template_name, {})
+    providers = Provider.objects.all()
+    return render(request, template_name, {'providers': providers})
 
 def offers(request, template_name='covfefe/offers.html'):
-    return render(request, template_name, {})
+    offers = Offer.objects.all()
+    return render(request, template_name, {'offers': offers})
