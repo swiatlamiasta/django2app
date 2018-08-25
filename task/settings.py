2f836e6e922b545d7a8e5c4b38c1ae66f9923cd3
swiatlamiasta <arturziomas@gmail.com>
2018-08-25 15:04:35 +0200

    setting up heroku postgres

--- task/settings.py
+++ task/settings.py
@@ -82,13 +82,22 @@ WSGI_APPLICATION = 'task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'desfl475ukpml5',
+        'USER': 'ixpyemffiizvlc',
+        'PASSWORD': 'ac04203b3d80f0deb59640a57a9c20b9c51374ccc8ca20958e29287c44a787ab',
+        'HOST': 'ec2-174-129-236-147.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -125,15 +134,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATICFILES_DIRS = [
-#     "../covfefe/static/covfefe/css"
-# ]
+
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
